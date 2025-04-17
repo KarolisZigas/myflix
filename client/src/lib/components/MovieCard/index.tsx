3abed0f73ecc8fa92ba167cdc2a06913e7c04818
Karolis <karolis@daisoras.lt>
2025-04-17 23:48:47 +0300

    add: movieCard changes, functionality

--- client/src/lib/components/MovieCard/index.tsx
+++ client/src/lib/components/MovieCard/index.tsx
@@ -1,24 +1,136 @@
-import React from "react"
-import { Card, Typography } from "antd";
-import { UserOutlined, EditOutlined, PlusCircleOutlined, EllipsisOutlined, LinkOutlined, StarFilled } from "@ant-design/icons";
-import { Movie, SavedMovie } from "../../../generated/graphql";
+import React, { useState } from "react"
+import { Card, Dropdown, Space, Typography } from "antd";
+import { UserOutlined, EditOutlined, PlusCircleOutlined, EllipsisOutlined, LinkOutlined, StarFilled, MinusCircleOutlined, ArrowRightOutlined } from "@ant-design/icons";
+import { DeleteMovieMutation, DeleteMovieMutationVariables, Movie, SavedMovie, SaveMovieMutation, SaveMovieMutationVariables } from "../../../generated/graphql";
+import { useMutation } from "@apollo/client";
+import { DELETE_MOVIE } from "../../graphql/mutations/DeleteMovie";
+import { useParams } from "react-router-dom";
+import { Spin } from 'antd';
+import { SAVE_MOVIE } from "../../graphql/mutations/SaveMovie";
+import { displaySuccessNotification } from "../../utils";
+import { MenuProps } from "antd";
 
 interface Props {
     movie: SavedMovie
 }
 
-const {Text, Title} = Typography;
+const { Text, Title } = Typography;
 const { Meta } = Card;
 
 export const MovieCard = ({ movie }: Props) => {
-    const { title, poster, rating, imdbId, releaseDate, description } = movie.movie
+    const params = useParams();
+    const [savedMovie, setSavedMovie] = useState(true);
+    const { title, poster, id, originalId, genres, rating, imdbId, releaseDate, description, isSaved } = movie.movie
+
+    const [deleteMovie,
+        {
+            loading: deleteMovieLoading, 
+            error: deleteMovieError,
+        }
+    ] = useMutation<DeleteMovieMutation, DeleteMovieMutationVariables>(DELETE_MOVIE, {
+        variables: {
+            movieId: originalId,
+            userId: params.id ?? ''
+        },
+        onCompleted: () => {
+            displaySuccessNotification('Movie deleted from the list!')
+        }
+    })
+
+    const [
+        saveMovie, 
+        {
+            loading,
+            error
+        }
+    ] = useMutation<SaveMovieMutation, SaveMovieMutationVariables>(SAVE_MOVIE, {
+        variables: {
+            movie: {
+                id: id,
+                title,
+                originalId,
+                imdbId,
+                description,
+                rating,
+                genres,
+                releaseDate,
+                poster
+            },
+            userId: params.id ?? ''
+        },
+        onCompleted: () => {
+            displaySuccessNotification('Movie saved to the list!')
+        }
+    })
+
+    const handleRemove = () => {
+        deleteMovie();
+        setSavedMovie(false);
+    }
+
+    const handleAdd = () => {
+        saveMovie();
+        setSavedMovie(true);
+    }
 
     const ratingElement = rating ? (
         <div className="flex gap-[6px]">
-            <span>{rating}</span>
+            <span>{rating.toFixed(1)}</span>
             <StarFilled className="[&>svg]:fill-yellow-300 [&>svg]:stroke-black"/>
         </div>
     ) : null
+
+    const saveElement = savedMovie ? (
+        <MinusCircleOutlined 
+            className="remove-item [&>svg]:fill-red-600"
+            onClick={handleRemove} 
+            key="remove"
+        />
+    ) : (
+        <PlusCircleOutlined
+            className="add-item [&>svg]:fill-green-600"
+            onClick={handleAdd} 
+            key='add'
+        />
+    )
+
+    const items: MenuProps['items'] = [
+        {
+            key: '1',
+            label: (
+                <a className="flex gap-[6px]" target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${imdbId}`}>
+                    Imdb 
+                    <ArrowRightOutlined />
+                </a>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <a className="flex gap-[6px]" target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${imdbId}`}>
+                    Leave a note
+                </a>
+            ),
+        },
+        {
+            key: '3',
+            label: (
+                <a className="flex gap-[6px]" target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${imdbId}`}>
+                    Discussion
+                </a>
+            ),
+        }
+    ]
+
+    const dropdownElement = (
+        <Dropdown menu={{ items }}>
+            <a href="/" onClick={(e) => e.preventDefault()}>
+                <Space>
+                    <EllipsisOutlined key="ellipsis"/>
+                </Space>
+            </a>
+        </Dropdown>
+    );
 
     return (
         // <Card 
@@ -44,29 +156,31 @@ export const MovieCard = ({ movie }: Props) => {
         //         </div>
         //     </div>
         // </Card>
-        <Card
-            classNames={{
-                header: 'my-card',
-                title: 'my-card'
-            }}
-            extra={ratingElement}
-            title={title}
-            style={{ width: 300 }}
-            cover={
-                <img 
-                    alt={`${poster}`}
-                    src={`https://image.tmdb.org/t/p/original/${poster}`}
+        <Spin spinning={deleteMovieLoading || loading}>
+            <Card
+                classNames={{
+                    header: 'my-card',
+                    title: 'my-card'
+                }}
+                extra={ratingElement}
+                title={title}
+                style={{ width: 300 }}
+                cover={
+                    <img 
+                        alt={`${poster}`}
+                        src={`https://image.tmdb.org/t/p/original/${poster}`}
+                    />
+                }
+                actions={[
+                    saveElement,
+                    <LinkOutlined key="edit"/>,
+                    dropdownElement
+                ]}
+            >
+                <Meta
+                    description={description}
                 />
-            }
-            actions={[
-                <PlusCircleOutlined key="save"/>,
-                <LinkOutlined key="edit"/>,
-                <EllipsisOutlined key="ellipsis"/>
-            ]}
-        >
-            <Meta
-                description={description}
-            />
-        </Card>
+            </Card>
+        </Spin>
     )
 }
